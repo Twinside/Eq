4b2ee28bce911ef0b21d9a2647a70502e73d94d6
Vincent Berthoux <twinside@gmail.com>
2009-08-10 16:54:55 +0200

    Forgot to add important file

---
+++ EqManips/Propreties.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+module EqManips.Propreties( Property( .. )
+                          , obtainProp
+                          ) where
+
+import Data.Maybe
+
+-- | Class to attach static propreties to a type
+-- minimum definition : getProps
+class (Eq propKey) => Property onType propKey propVal 
+        | propKey -> propVal where
+    -- | To retrieve all the propreties
+    -- of the current item
+    getProps :: onType -> [(propKey, propVal)] 
+
+    -- | retrieve a propretie if it exists
+    getProp :: onType -> propKey -> Maybe propVal
+    getProp a what = lookup what $ getProps a
+    
+    -- | Tell if the element as the propreties
+    -- passed as parameters
+    hasProp :: onType -> propKey -> Bool
+    hasProp a p = case getProp a p of
+        Nothing -> False
+        Just _ -> True
+
+                                 
+obtainProp :: (Property a p c) => a -> p -> c
+obtainProp a p = fromJust $ getProp a p
+
