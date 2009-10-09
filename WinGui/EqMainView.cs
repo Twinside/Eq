bf6495fe832204a349d1a6f0d127b8fc545c7529
Vincent Berthoux <twinside@gmail.com>
2009-10-09 22:21:30 +0200

    Toying with Windows 7 Math input

--- WinGui/EqMainView.cs
+++ WinGui/EqMainView.cs
@@ -6,16 +6,36 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+using micautLib;
+
 namespace WinGui
 {
     public partial class EqMainView : Form
     {
         private EqBridge computationKernel;
+        private MathInputControl mathInput;
 
         public EqMainView(EqBridge kernel)
         {
             InitializeComponent();
+
+            mathInput = new MathInputControl();
+            mathInput.SetOwnerWindow(rezEntrySplitter.Panel1.Handle.ToInt32());
+            mathInput.Show();
+            mathInput.Insert += new _IMathInputControlEvents_InsertEventHandler(mathInput_Insert);
+            mathInput.Close += new _IMathInputControlEvents_CloseEventHandler(mathInput_Close);
+
             computationKernel = kernel;
+        }
+
+        void mathInput_Close()
+        {
+            mathInput.Hide();
+        }
+
+        void mathInput_Insert(string RecoResult)
+        {
+            MessageBox.Show(RecoResult);
         }
 
         private void txtEntry_KeyUp(object sender, KeyEventArgs e)
