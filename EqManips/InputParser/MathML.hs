dad579dbef920da42a068fa56d7e19164180376e
Vincent Berthoux <twinside@gmail.com>
2009-10-10 17:11:25 +0200

    Continuing import of MathML

--- EqManips/InputParser/MathML.hs
+++ EqManips/InputParser/MathML.hs
@@ -1,8 +1,12 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module EqManips.InputParser.MathML ( mathMlToEqLang
                                    , mathMlToEqLang'
                                    ) where
 
-import EqManips.Types
+import Data.Either
+import Control.Applicative
+import EqManips.Algorithm.Utils
+import qualified EqManips.UnicodeSymbols as Uni
 
 import Text.XML.HaXml.Parse
 import Text.XML.HaXml.Types
@@ -16,30 +20,31 @@ data ReducedXmlTree =
     | Xsqrt ReducedXmlTree
     | Xfrac ReducedXmlTree ReducedXmlTree
     | Xsup ReducedXmlTree ReducedXmlTree
+    | XunderOver ReducedXmlTree ReducedXmlTree ReducedXmlTree
     | Xrow [ReducedXmlTree]
+    | Xtable [[ReducedXmlTree]]
     deriving (Show)
 
 mathMlToEqLang' :: String -> String
-mathMlToEqLang' txt = maybe "" id (mathMlToEqLang txt)
+mathMlToEqLang' txt = either (const "") id (mathMlToEqLang txt)
 
 -- | Input XML code encoded in a string
 -- output a string in Eq Language, ready to
 -- be parsed by the usual meanings.
-mathMlToEqLang :: String -> Maybe String
-mathMlToEqLang = either (const Nothing) 
-                        (Just . toProgramString . simplifyXml)
-               . xmlParse' "mathml"
+mathMlToEqLang :: String -> Either String String
+mathMlToEqLang text =
+    xmlParse' "mathml" text >>= simplifyXml >>= toProgramString
 
-toProgramString :: ReducedXmlTree -> String
-toProgramString _ = ""
+toProgramString :: ReducedXmlTree -> Either String String
+toProgramString tree = (\s -> s "") <$> translate tree
 
-simplifyXml :: Document a -> ReducedXmlTree
+simplifyXml :: Document a -> Either String ReducedXmlTree
 simplifyXml (Document _ _ (Elem "math" _ lst) _) =
-    Xrow $ map simplifyContent lst
+    Xrow <$> (eitherMap $ map simplifyContent lst)
 simplifyXml _ = error "The xml document has the wrong format"
 
 strOfContent :: Content a -> String
-strOfContent (CString _ str _) = str
+strOfContent (CString _ txt _) = txt
 strOfContent _ = error "Xml string waited at this point"
 
 elemOfContent :: Content a -> Element a
@@ -47,20 +52,54 @@ elemOfContent (CElem e _) = e
 elemOfContent _ = error "Xml element waited at this point"
 
 -- | Helper to simplify content
-simplifyContent :: Content a -> ReducedXmlTree
+simplifyContent :: Content a -> Either String ReducedXmlTree
 simplifyContent = simplify . elemOfContent
+
+instance Applicative (Either a) where
+    pure = Right
+    (<*>) (Left a) _ = Left a
+    (<*>) (Right _) (Left b) = Left b
+    (<*>) (Right f) (Right v) = Right (f v)
+
+instance Monad (Either a) where
+    return = Right
+    (>>=) (Left a) _ = Left a
+    (>>=) (Right v) f = f v
+
+eitherMap :: [Either a b] -> Either a [b]
+eitherMap [] = Right []
+eitherMap lst = foldr mapper (Right []) lst
+    where mapper (Left a) _ = Left a
+          mapper _ (Left a) = Left a
+          mapper (Right v) (Right list) = Right (v:list)
 
 -- | Really transform an XML file to a simplified tree
 -- to make a better pattern matching
-simplify :: Element a -> ReducedXmlTree
-simplify (Elem "mi" _ [c]) = Xsymb $ strOfContent c
-simplify (Elem "mn" _ [c]) = Xnum $ strOfContent c
-simplify (Elem "mo" _ [c]) = Xop $ strOfContent c
-simplify (Elem "mrow" _ lst) = Xrow $ map simplifyContent lst
-simplify (Elem "msqrt" _ lst) = Xsqrt . Xrow $ map simplifyContent lst
-simplify (Elem "mfrac" _ [a,b]) = Xfrac (simplifyContent a) (simplifyContent b)
-simplify (Elem "msup" _ [a,b]) = Xsup (simplifyContent a) (simplifyContent b)
-simplify (Elem elemName _ _) = error $ "Unknown MathMl element : " ++ elemName
+simplify :: Element a -> Either String ReducedXmlTree
+simplify (Elem "mi" _ [c]) = Right . Xsymb $ strOfContent c
+simplify (Elem "mn" _ [c]) = Right . Xnum $ strOfContent c
+simplify (Elem "mo" _ [c]) = Right . Xop $ strOfContent c
+simplify (Elem "mrow" _ lst) = Xrow <$> (eitherMap $ map simplifyContent lst)
+simplify (Elem "msqrt" _ lst) = Xsqrt . Xrow <$> eitherMap (map simplifyContent lst)
+simplify (Elem "mfrac" _ [a,b]) = Xfrac <$> simplifyContent a <*> simplifyContent b
+simplify (Elem "msup" _ [a,b]) = Xsup <$> simplifyContent a <*> simplifyContent b
+simplify (Elem "munderover" _ [a,b,c]) = 
+    XunderOver <$> simplifyContent a <*> simplifyContent b <*> simplifyContent c
+
+simplify (Elem "mtable" _ lst) = Xtable <$> lineList
+    where lineList = eitherMap $ map (unrow . elemOfContent) lst
+
+          unrow (Elem "mtr" _ cells) = eitherMap $ map (uncell . elemOfContent) cells
+          unrow _ = Left "Ill formed MathML Matrix"
+
+          uncell (Elem "mtd" _ [cell]) = simplifyContent cell
+          uncell _ = Left "Ill format MathML Matrix cell"
+
+simplify (Elem "mfenced" [ ("open", AttValue [Left "("])
+                         , ("close", AttValue [Left "("]) ] lst) =
+    Xrow <$> (eitherMap $ map simplifyContent lst)
+    
+simplify (Elem elemName _ _) = Left $ "Unknown MathMl element : " ++ elemName
 
 str :: String -> String -> String
 str = (++)
@@ -68,15 +107,35 @@ str = (++)
 char :: Char -> String -> String
 char = (:)
 
--- | Real transformation =)
-translate :: ReducedXmlTree -> ShowS
-translate (Xop s) = str s
-translate (Xsymb s) = str s
-translate (Xnum s) = str s
-translate (Xsqrt subTree) = str "sqrt(" . translate subTree . char ')'
-translate (Xfrac a b) =
-    char '(' . translate a . str ") / (" . translate b . char ')'
-translate (Xsup a b) =
-    char '(' . translate a . str ") ^ (" . translate b . char ')'
-translate (Xrow lst) = char '(' . concatMapS translate lst . char ')'
+unicodeTranslation :: [(Int, String)]
+unicodeTranslation =
+    [ (Uni.logicalAnd, "&&")
+    , (Uni.logicalOr, "||")
+    , (Uni.identicalTo, "==")
+    , (Uni.lessThanOrEqualTo, "<=")
+    , (Uni.greaterThanOrEqualTo, ">=")
+    ]
 
+-- | Real transformation =)
+translate :: ReducedXmlTree -> Either String ShowS
+translate (Xop s) = Right $ str s
+translate (Xsymb s) = Right $ str s
+translate (Xnum s) = Right $ str s
+translate (Xsqrt subTree) = (\sub -> str "sqrt(" . sub . char ')')
+                         <$> translate subTree 
+translate (Xfrac a b) = (\a' b' -> char '(' . a' . str ") / (" . b' . char ')')
+                     <$> translate a 
+                     <*> translate b
+translate (Xsup a b) = (\a' b' -> char '(' . a' . str ") ^ (" . b' . char ')')
+                    <$> translate a 
+                    <*> translate b
+translate (Xrow lst) =
+    (\translist -> char '(' . concatS translist . char ')') <$> eitherMap (map translate lst)
+
+translate (Xtable []) = Left "Wrong table format"
+translate (Xtable lst) =
+    (\elems -> str "matrix( " . shows lineCount . char ',' . shows columncount . char ','
+                              . interspereseS (char ',') elems . char ')')
+        <$> (eitherMap . map translate $ concat lst) 
+    where lineCount = length lst
+          columncount = length $ head lst
---
+++ EqManips/UnicodeSymbols.hs
@@ -0,0 +1,335 @@
+﻿module EqManips.UnicodeSymbols where
+
+------------------------------------
+-- Miscellaneou mathematical symbols
+------------------------------------
+forAll    = 0x2200 -- ^ ∀
+exist     = 0x2203 -- ^ ∃
+notExist  = 0x2204 -- ^ ∄
+
+empty     = 0x2205 -- ^ ∅
+increment = 0x2206 -- ^ ∆
+nabla     = 0x2207 -- ^ ∇
+
+-----------------------------------
+-- Set membership
+-----------------------------------
+elementof      = 0x2208 -- ^ ∈
+notelementof   = 0x2209 -- ^ ∉
+smallelementof = 0x220A -- ^ ∊
+contains       = 0x220b -- ^ ∋
+smallcontains  = 0x220D -- ^ ∍
+
+-----------------------------------
+-- N-ary operators
+----------------------------------
+product   = 0x220F -- ^ ∏
+coproduct = 0x2210 -- ^ ∐
+sum       = 0x2211 -- ^ ∑
+
+-----------------------------------
+-- Simple operators
+-----------------------------------
+minus          = 0x2212 -- ^ −
+minusorplus    = 0x2213 -- ^ ∓
+dotplus        = 0x2214 -- ^ ∔
+divsplash      = 0x2215 -- ^ ∕
+setminus       = 0x2216 -- ^ ∖
+asterisk       = 0x2217 -- ^ ∗
+ring           = 0x2218 -- ^ ∘
+bullet         = 0x2219 -- ^ ∙
+squareroot     = 0x221A -- ^ √
+cuberoot       = 0x221B -- ^ ∛
+fouthroot      = 0x221C -- ^ ∜
+proportionalto = 0x221D -- ^ ∝
+
+
+-----------------------------------
+-- Miscellaneous math symbols
+-----------------------------------
+infinity       = 0x221E -- ^ ∞
+rightangle     = 0x221F -- ^ ∟
+angle          = 0x2220 -- ^ ∠
+measuredangle  = 0x2221 -- ^ ∡
+sphericalangle = 0x2222 -- ^ ∢
+
+-----------------------------------
+-- Operators 2 the return
+-----------------------------------
+divides      = 0x2223 -- ^ ∣
+doesntdivide = 0x2224 -- ^ ∤
+parrallelto  = 0x2225 -- ^ ∥
+unparallelto = 0x2226 -- ^ ∦
+
+-----------------------------------
+-- Logical and sets operators
+-----------------------------------
+logicalNot   = 0x00AC -- ^ ¬
+logicalAnd   = 0x2227 -- ^ ∧
+logicalOr    = 0x2228 -- ^ ∨
+intersection = 0x2229 -- ^ ∩
+union        = 0x222A -- ^ ∪
+
+
+-----------------------------------
+-- Integrals
+-----------------------------------
+integral                     = 0x222B -- ^ ∫
+integralDouble               = 0x222C -- ^ ∬
+integralTriple               = 0x222D -- ^ ∭
+contourIntegral              = 0x222E -- ^ ∮
+surfaceIntegral              = 0x222F -- ^ ∯
+volumeIntegral               = 0x2230 -- ^ ∰
+clockwiseIntegral            = 0x2231 -- ^ ∱
+clockwiseCountourIntegral    = 0x2232 -- ^ ∲
+anticlockWiseContourIntegral = 0x2233 -- ^ ∳
+
+-- Misc math symbols
+therefor = 0x2234 -- ^ ∴
+because  = 0x2235 -- ^ ∵
+
+-- Relatioons
+ratio      = 0x2236 -- ^ ∶
+proportion = 0x2237 -- ^ ∷
+
+-- operator
+dotMinus = 0x2238 -- ^ ∸
+
+-- Relation
+excess = 0x2239 -- ^ ∹
+
+-- Operator
+geometricProportion = 0x223A -- ^ ∺
+
+-----------------------------------
+-- Relations
+-----------------------------------
+Homothetic    = 0x223B -- ^ ∻
+Tilde         = 0x223C -- ^ ∼
+ReversedTilde = 0x223D -- ^ ∽
+InvertedLazys = 0x223E -- ^ ∾
+
+-- Misc math symbol
+sineWave = 0x223F -- ^ ∿
+
+-- Operator
+wreathProduct             = 0x2240 -- ^ ≀
+notTilde                  = 0x2241 -- ^ ≁
+minusTilde                = 0x2242 -- ^ ≂
+asymEqualTo               = 0x2243 -- ^ ≃
+notAsymEqualTo            = 0x2244 -- ^ ≄
+aproxEqualTo              = 0x2245 -- ^ ≅
+aproxButNotEqualTo        = 0x2246 -- ^ ≆
+neitherAproxNorEqual      = 0x2247 -- ^ ≇
+almostEqual               = 0x2248 -- ^ ≈
+notAlmostEqual            = 0x2249 -- ^ ≉
+almostEqualorEqual        = 0x224A -- ^ ≊
+tripleTilde               = 0x224B -- ^ ≋
+allEqualTo                = 0x224C -- ^ ≌
+equavalent                = 0x224D -- ^ ≍
+geomEquiv                 = 0x224E -- ^ ≎
+diffBetween               = 0x224F -- ^ ≏
+approachLimit             = 0x2250 -- ^ ≐
+geomEqual                 = 0x2251 -- ^ ≑
+aproxEqual                = 0x2252 -- ^ ≒
+imageOf                   = 0x2253 -- ^ ≓
+colonEquals               = 0x2254 -- ^ ≔
+equalsColon               = 0x2255 -- ^ ≕
+ringInEqual               = 0x2256 -- ^ ≖
+ringEqualTo               = 0x2257 -- ^ ≗
+correspondsTo             = 0x2258 -- ^ ≘
+Estimates                 = 0x2259 -- ^ ≙
+EquiangularTo             = 0x225A -- ^ ≚
+starEquals                = 0x225B -- ^ ≛
+deltaEqual                = 0x225C -- ^ ≜
+equalByDef                = 0x225D -- ^ ≝
+measuredBy                = 0x225E -- ^ ≞
+questionedEqualTo         = 0x225F -- ^ ≟
+notEqualTo                = 0x2260 -- ^ ≠
+identicalTo               = 0x2261 -- ^ ≡
+notIdenticalTo            = 0x2262 -- ^ ≢
+strictlyEquivalentTo      = 0x2263 -- ^ ≣
+lessThanOrEqualTo         = 0x2264 -- ^ ≤
+greaterThanOrEqualTo      = 0x2265 -- ^ ≥
+lessThanOverEqualTo       = 0x2266 -- ^ ≦
+greaterThanOverEqualTo    = 0x2267 -- ^ ≧
+lessThanButNotEqual       = 0x2268 -- ^ ≨
+greaterThanButnotEqualTo  = 0x2269 -- ^ ≩
+muchLessThan              = 0x226A -- ^ ≪
+muchGreaterThan           = 0x226B -- ^ ≫
+between                   = 0x226C -- ^ ≬
+notEquivalentTo           = 0x226D -- ^ ≭
+notLessThan               = 0x226E -- ^ ≮
+notGreaterThan            = 0x226F -- ^ ≯
+neitherLessThanNorEqualTo = 0x2270 -- ^ ≰
+subset                    = 0x2282 -- ^ ⊂
+superset                  = 0x2283 -- ^ ⊃
+notASubset                = 0x2284 -- ^ ⊄
+notASuperset              = 0x2285 -- ^ ⊅
+subsetOrEqualTo           = 0x2286 -- ^ ⊆
+superSetOrEqual           = 0x2287 -- ^ ⊇
+neitherSubsetNorEqual     = 0x2288 -- ^ ⊈
+neitherSupersetNorEqual   = 0x2289 -- ^ ⊉
+subsetWithNotEqual        = 0x228A -- ^ ⊊
+supersetofWithNotEqual    = 0x228B -- ^ ⊋
+
+-- operators
+multiset      = 0x228C -- ^ ⊌
+multisetMult  = 0x228D -- ^ ⊍
+multisetUnion = 0x228E -- ^ ⊎
+
+-- greek letters
+alpha = 0x03B1 -- ^ α
+beta = 0x03B2 -- ^ β
+chi = 0x03C7 -- ^ χ
+gamma = 0x3B3 -- ^ γ
+delta = 0x03B4 -- ^ δ
+epslion = 0x03B6 -- ^ ε
+theta = 0x3B8 -- ^ θ
+pi = 0x03C0 -- ^ π
+rho = 0x03C1 -- ^ ρ
+phi = 0x03C6 -- ^ φ
+tau = 0x03C4 -- ^ τ
+omega = 0x03C9 -- ^ ω
+lambda = 0x03BB -- ^ λ
+sigma = 0x03C3 -- ^ σ
+mu = 0x03BC -- ^ μ
+psi = 0x03C8 -- ^ ψ
+
+-- Relation
+ = 0x228F -- ^ ⊏
+ = 0x2290 -- ^ ⊐
+ = 0x2291 -- ^ ⊑
+ = 0x2292 -- ^ ⊒
+ = 0x2293 -- ^ ⊓
+ = 0x2294 -- ^ ⊔
+ = 0x2295 -- ^ ⊕
+ = 0x2296 -- ^ ⊖
+ = 0x2297 -- ^ ⊗
+ = 0x2298 -- ^ ⊘
+ = 0x2299 -- ^ ⊙
+ = 0x229A -- ^ ⊚
+ = 0x229B -- ^ ⊛
+ = 0x229C -- ^ ⊜
+ = 0x229D -- ^ ⊝
+ = 0x229E -- ^ ⊞
+ = 0x229F -- ^ ⊟
+ = 0x22A0 -- ^ ⊠
+ = 0x22A1 -- ^ ⊡
+ = 0x22A2 -- ^ ⊢
+ = 0x22A3 -- ^ ⊣
+ = 0x22A4 -- ^ ⊤
+ = 0x22A5 -- ^ ⊥
+ = 0x22A6 -- ^ ⊦
+ = 0x22A7 -- ^ ⊧
+ = 0x22A8 -- ^ ⊨
+ = 0x22A9 -- ^ ⊩
+ = 0x22AA -- ^ ⊪
+ = 0x22AB -- ^ ⊫
+ = 0x22AC -- ^ ⊬
+ = 0x22AD -- ^ ⊭
+ = 0x22AE -- ^ ⊮
+ = 0x22AF -- ^ ⊯
+ = 0x22B0 -- ^ ⊰
+ = 0x22B1 -- ^ ⊱
+ = 0x22B2 -- ^ ⊲
+ = 0x22B3 -- ^ ⊳
+ = 0x22B4 -- ^ ⊴
+ = 0x22B5 -- ^ ⊵
+ = 0x22B6 -- ^ ⊶
+ = 0x22B7 -- ^ ⊷
+ = 0x22B8 -- ^ ⊸
+ = 0x22B9 -- ^ ⊹
+ = 0x22BA -- ^ ⊺
+xor = 0x22BB -- ^ ⊻
+ = 0x22BC -- ^ ⊼
+ = 0x22BD -- ^ ⊽
+ = 0x22BE -- ^ ⊾
+ = 0x22BF -- ^ ⊿
+ = 0x22C0 -- ^ ⋀
+ = 0x22C1 -- ^ ⋁
+ = 0x22C2 -- ^ ⋂
+ = 0x22C3 -- ^ ⋃
+ = 0x22C4 -- ^ ⋄
+ = 0x22C5 -- ^ ⋅
+ = 0x22C6 -- ^ ⋆
+ = 0x22C7 -- ^ ⋇
+ = 0x22C8 -- ^ ⋈
+ = 0x22C9 -- ^ ⋉
+ = 0x22CA -- ^ ⋊
+ = 0x22CB -- ^ ⋋
+ = 0x22CC -- ^ ⋌
+ = 0x22CD -- ^ ⋍
+ = 0x22CE -- ^ ⋎
+ = 0x22CF -- ^ ⋏
+ = 0x22D0 -- ^ ⋐
+ = 0x22D1 -- ^ ⋑
+ = 0x22D2 -- ^ ⋒
+ = 0x22D3 -- ^ ⋓
+ = 0x22D4 -- ^ ⋔
+ = 0x22D5 -- ^ ⋕
+ = 0x22D6 -- ^ ⋖
+ = 0x22D7 -- ^ ⋗
+ = 0x22D8 -- ^ ⋘
+ = 0x22D9 -- ^ ⋙
+ = 0x22DA -- ^ ⋚
+ = 0x22DB -- ^ ⋛
+ = 0x22DC -- ^ ⋜
+ = 0x22DD -- ^ ⋝
+ = 0x22DE -- ^ ⋞
+ = 0x22DF -- ^ ⋟
+ = 0x22E0 -- ^ ⋠
+ = 0x22E1 -- ^ ⋡
+ = 0x22E2 -- ^ ⋢
+ = 0x22E3 -- ^ ⋣
+ = 0x22E4 -- ^ ⋤
+ = 0x22E5 -- ^ ⋥
+ = 0x22E6 -- ^ ⋦
+ = 0x22E7 -- ^ ⋧
+ = 0x22E8 -- ^ ⋨
+ = 0x22E9 -- ^ ⋩
+ = 0x22EA -- ^ ⋪
+ = 0x22EB -- ^ ⋫
+ = 0x22EC -- ^ ⋬
+ = 0x22ED -- ^ ⋭
+ = 0x22EE -- ^ ⋮
+ = 0x22EF -- ^ ⋯
+ = 0x22F0 -- ^ ⋰
+ = 0x22F1 -- ^ ⋱
+ = 0x22F2 -- ^ ⋲
+ = 0x22F3 -- ^ ⋳
+ = 0x22F4 -- ^ ⋴
+ = 0x22F5 -- ^ ⋵
+ = 0x22F6 -- ^ ⋶
+ = 0x22F7 -- ^ ⋷
+ = 0x22F8 -- ^ ⋸
+ = 0x22F9 -- ^ ⋹
+ = 0x22FA -- ^ ⋺
+ = 0x22FB -- ^ ⋻
+ = 0x22FC -- ^ ⋼
+ = 0x22FD -- ^ ⋽
+ = 0x22FE -- ^ ⋾
+ = 0x22FF -- ^ ⋿
+
+{-
+Dump for others chars, to lazy to prepare them    
+ = 0x2271 -- ^ ≱
+ = 0x2272 -- ^ ≲
+ = 0x2273 -- ^ ≳
+ = 0x2274 -- ^ ≴
+ = 0x2275 -- ^ ≵
+ = 0x2276 -- ^ ≶
+ = 0x2277 -- ^ ≷
+ = 0x2278 -- ^ ≸
+ = 0x2279 -- ^ ≹
+ = 0x227A -- ^ ≺
+ = 0x227B -- ^ ≻
+ = 0x227C -- ^ ≼
+ = 0x227D -- ^ ≽
+ = 0x227E -- ^ ≾
+ = 0x227F -- ^ ≿
+ = 0x2280 -- ^ ⊀
+ = 0x2281 -- ^ ⊁
+
+ --}
+
