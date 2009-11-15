151092cc89243491a3ac1c0d06530da64eb3c3b2
Vincent Berthoux <twinside@gmail.com>
2009-11-15 14:07:23 +0100

    Added polynome generator for random testing

--- EqManips/Tests/FullGenerator.hs
+++ EqManips/Tests/FullGenerator.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-orphans -fno-warn-missing-methods #-}
 module EqManips.Tests.FullGenerator( formulaGen ) where
 
+import Data.Ratio
 import Control.Applicative
 import Control.Monad
 import Test.QuickCheck
@@ -29,6 +31,13 @@ instance Arbitrary UnOperator where
 instance Arbitrary FormulaPrim where
     arbitrary = formulaGen 1
 
+instance Arbitrary PolyCoeff where
+    arbitrary = do
+        n :: Int <- choose (0,1)
+        case n of
+             0 -> CoeffInt <$> choose (1,150)
+             1 -> (\a b -> CoeffRatio $ a % b) <$> choose (1,150) <*> choose (1,150)
+             _ -> error "Not permited"
 
 instance Arbitrary VarLetter where
     arbitrary = do
@@ -57,11 +66,24 @@ formulaGen n
                  , Integrate <$> subFormul <*> subFormul 
                              <*> subFormul <*> subFormul
                  , App <$> subFormul <*> formulist
+                 , Poly <$> polynomeGenerator (n-1)
                  , matrixGenerator (n-1)
                  ]
           where subFormul = formulaGen (n-1)
                 formulist = do genCount <- choose (2, 7)
                                replicateM genCount subFormul
+
+polynomeGenerator :: Int -> Gen Polynome
+polynomeGenerator i | i < 0 = PolyRest <$> arbitrary
+polynomeGenerator i = do
+    end :: Int <- choose (1,2)
+    if end == 1
+       then PolyRest <$> arbitrary
+       else do
+        count <- choose (1,10)
+        body <- replicateM count ((,) <$> arbitrary <*> polynomeGenerator (i-1))
+        (VarLetter v) <- arbitrary
+        return $ Polynome [v] body
 
 matrixGenerator :: Int -> Gen FormulaPrim
 matrixGenerator deep = do
