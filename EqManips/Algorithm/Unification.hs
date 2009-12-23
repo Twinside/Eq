b119f56a63b692cf177bb064e8bc76c9282a273b
Vincent Berthoux <twinside@gmail.com>
2009-12-24 00:45:45 +0100

    Unification polynome/polynome added

--- EqManips/Algorithm/Unification.hs
+++ EqManips/Algorithm/Unification.hs
@@ -48,10 +48,10 @@ unify (Formula a) (Formula b) =
 -- Used for "tuples"/arguments
 unifyList :: [FormulaPrim] -> [FormulaPrim] -> UnificationContext Bool
 unifyList l1 l2 
-	| length l1 == length l2 =
-		let valid acc (a,b) = (acc &&) <$> (a =~= b)
-		in foldM valid True $ zip l1 l2
-	| otherwise = return False
+    | length l1 == length l2 =
+        let valid acc (a,b) = (acc &&) <$> (a =~= b)
+        in foldM valid True $ zip l1 l2
+    | otherwise = return False
 
 -- | Real function that implement unification.
 -- origin pattern (function args...), to unify
@@ -61,6 +61,36 @@ unifyFormula :: FormulaPrim -- ^ Pattern
 unifyFormula (App f1 l1) (App f2 l2) =
     (&&) . valid <$> (f1 =~= f2) <*> unifyList l1 l2
         where valid = (&&) $ length l1 == length l2 
+
+unifyFormula (Fraction f1) (Fraction f2) =
+    return $ f1 == f2
+
+unifyFormula (Complex (re, im)) (Complex (re2, im2)) =
+    (&&) <$> (re =~= re2) <*> (im =~= im2)
+
+unifyFormula (Poly left@(Polynome _ _))
+             (Poly right@(Polynome _ _)) =
+  if valid then and <$> mapM (uncurry checkSymbol) subs
+           else return False
+    where (subs, valid) = subPolyEq left right
+          subPolyEq (PolyRest a) (PolyRest b)   = ([], a == b)
+          subPolyEq (PolyRest _) (Polynome _ _) = ([], False)
+          subPolyEq (Polynome _ _) (PolyRest _) = ([], False)
+          -- Equals at an alpha renaming
+          subPolyEq (Polynome var1 lst1')
+                    replacement@(Polynome _ lst2') =
+                if valid' then ((var1, Poly replacement) : substitu, True)
+                          else ([], False)
+            where (substitu, valid') = verifyCoeff lst1' lst2'
+
+          verifyCoeff a b = foldr coefEq ([], True) $ zip a b
+
+          coefEq ((c1,sub1),(c2,sub2)) (alphas, acc) =
+              (alphas ++ alpha', acc && c1 == c2 && acc')
+                  where (alpha', acc') = subPolyEq sub1 sub2
+
+unifyFormula (BinOp OpAdd _added) (Poly (Polynome _v _lst)) =
+    return False
 
 unifyFormula (Truth a) (Truth b) =
     return $ a == b
@@ -79,14 +109,17 @@ unifyFormula (BinOp op1 l1) (BinOp op2 l2)
     | otherwise = return False
 
 unifyFormula (UnOp op1 f1) (UnOp op2 f2) =
-    ((&&) $ op1 == op2) <$> (f1 =~= f2)
+    (op1 == op2 &&) <$> (f1 =~= f2)
 
-unifyFormula (Variable v1) f2 = do
-    symbolList <- get
-    maybe (do put $ (v1,f2) : symbolList
-              return True)
-          (return . (f2 ==))
-          $ lookup v1 symbolList
+unifyFormula (Variable v1) f2 = checkSymbol v1 f2
 
 unifyFormula _ _ = return False
+
+checkSymbol :: String -> FormulaPrim -> UnificationContext Bool
+checkSymbol var what = do
+    symbolList <- get
+    maybe (do put $ (var, what) : symbolList
+              return True)
+          (return . (what ==))
+          $ lookup var symbolList
 
