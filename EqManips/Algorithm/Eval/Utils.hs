4e8444ba2ec4e8f8272dbc3c86d860ed1722e0ee
Vincent Berthoux <twinside@gmail.com>
2010-04-05 17:57:48 +0200

    Fixed some problem of appearing parenthesis

--- EqManips/Algorithm/Eval/Utils.hs
+++ EqManips/Algorithm/Eval/Utils.hs
@@ -1,14 +1,17 @@
-module EqManips.Algorithm.Eval.Utils where
+module EqManips.Algorithm.Eval.Utils( left
+                                    , right
+                                    , binOpReducer
+                                    , binEval
+                                    ) where
 
 import Control.Applicative
+import Data.List( sort, foldl' )
 
 import EqManips.Types
 import EqManips.EvaluationContext
 import EqManips.Algorithm.Eval.Types
 import EqManips.Algorithm.Utils
 import EqManips.Propreties
-
-import Data.List( sort )
 
 left :: (Monad m) => a -> m (Either a b)
 left = return . Left
@@ -22,6 +25,15 @@ binOpReducer :: BinOperator -> [FormulaPrim] -> FormulaPrim
 binOpReducer _ [x] = x
 binOpReducer op lst = binOp op lst
 
+-- | Assuming children in list form, parse the list to 
+-- keep the general listform.
+binListRepacker :: BinOperator -> [FormulaPrim] -> FormulaPrim
+binListRepacker op lst = binOpReducer op
+                       $ foldl' emergeSubOp id lst []
+    where emergeSubOp acc (BinOp _ op2 subLst)
+                | op == op2 = acc . (subLst ++)
+          emergeSubOp acc sub = acc . (sub:)
+
 -- | Evaluate a binary operator
 binEval :: BinOperator -> EvalOp -> EvalOp -> [FormulaPrim] -> EqContext FormulaPrim
 binEval op f inv formulaList 
@@ -29,11 +41,11 @@ binEval op f inv formulaList
 #ifdef _DEBUG
         addTrace ("Sorting => ", treeIfyFormula . Formula $ binOp op formulaList) >>
 #endif
-        binOpReducer op <$> biAssocM f inv (sort formulaList)
+        binListRepacker op <$> biAssocM f inv (sort formulaList)
 
     | otherwise =
 #ifdef _DEBUG
         addTrace ("Basic Eval=>", treeIfyFormula . Formula $ binOp op formulaList) >>
 #endif
-        binOpReducer op <$> biAssocM f inv formulaList
+        binListRepacker op <$> biAssocM f inv formulaList
 
